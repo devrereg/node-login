dbb8673b0b7ef3febcf35c10c8444f79f199c3d5
andrewbigc <andrew@bigc.im>
2023-03-08 20:34:48 +0900

    git 초기화

---
+++ routes/home/index.js
@@ -0,0 +1,14 @@
+"use strict"
+const express =require("express");
+
+const router = express.Router();
+
+// 컨트롤러
+const ctrl = require("./home.ctrl")
+
+router.get('/', ctrl.home);
+
+router.get('/login',ctrl.login)
+
+
+module.exports = router;
